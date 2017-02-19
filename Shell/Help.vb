4909bd5db51034309e901a8fc11813011a8ca284
LunaSoft <lunasoftcorp2@gmail.com>
2017-02-19 21:00:22 +0000

    Add files via upload

---
+++ Shell/Help.vb
@@ -0,0 +1,12 @@
+﻿Module Help
+    Sub Main()
+        System.Console.WriteLine("")
+        System.Console.WriteLine("LS-DOS Help")
+        System.Console.WriteLine("=================================================")
+        System.Console.WriteLine("Help       | Shows all of the commands in LS-DOS.")
+        System.Console.WriteLine("cls        | Clears the screen.")
+        System.Console.WriteLine("calc       | The LS-DOS Calculator.")
+        System.Console.WriteLine("")
+        Terminal.Main()
+    End Sub
+End Module
---
+++ Shell/Terminal.vb
@@ -0,0 +1,27 @@
+﻿Module Terminal
+    'Variables
+    Dim Input As String
+    Sub Main()
+        System.Console.ForegroundColor = ConsoleColor.White
+        System.Console.Write("Command)")
+        Input = System.Console.ReadLine()
+        If Input = "" Then
+            Main()
+        End If
+        If Input.ToLower = "help" Then
+            Help.Main()
+        End If
+        If Input.ToLower = "cls" Then
+            Console.Clear()
+            Main()
+        End If
+        If Input.ToLower = "calc" Then
+            Calculator.Main()
+        End If
+        Console.WriteLine("")
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine("Error: Invalid Command")
+        Console.WriteLine("")
+        Main()
+    End Sub
+End Module
